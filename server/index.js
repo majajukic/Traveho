3e555195c952559ae368d0dc58ae81e0f4900615
majajukic <73577538+majajukic@users.noreply.github.com>
2021-05-12 15:05:23 +0200

    security implementation

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import postRoutes from './routes/posts.js';
-
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json({limit: "30mb", extended: true}))//limit for images.
 app.use(express.urlencoded({extended: true}));
@@ -12,10 +14,10 @@ app.use(cors());//this has to go above the routes.
 app.use('/posts', postRoutes);//every route with postRoutes is going to start with 'posts'.
 
 //Connection to the database:
-const CONNECTION_URL = 'mongodb+srv://majajukic:majajukic111@cluster0.4ask5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://majajukic:majajukic111@cluster0.4ask5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;//soft-coded port for the case of app deployment and hardcoded option.
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})//to prevent warnings in the console.
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})//to prevent warnings in the console.
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
 mongoose.set('useFindAndModify', false);//also prevents warnings in the console (not necessary).
