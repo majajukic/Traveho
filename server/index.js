41812ede564e4de9e059d0c5013dd19239256208
majajukic <73577538+majajukic@users.noreply.github.com>
2021-05-12 16:27:33 +0200

    ispravka

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors());//this has to go above the routes.
 app.use('/posts', postRoutes);//every route with postRoutes is going to start with 'posts'.
 
 //Connection to the database:
-//const CONNECTION_URL = 'mongodb+srv://majajukic:majajukic111@cluster0.4ask5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;//soft-coded port for the case of app deployment and hardcoded option.
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})//to prevent warnings in the console.
